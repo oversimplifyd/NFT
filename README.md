ecfe39517ef58a06f382475fc96b4f23d743c93d
Arotimi Busayo <arotimi.busayo@gmail.com>
2022-06-30 20:08:27 +0200

    Adds NFT links to Readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NFT Domain System on Polygon
+[View on Opensea Testnet](https://testnets.opensea.io/collection/ginger-name-service-v3)
+[View Contract on Mumba Polygon Testnet](https://mumbai.polygonscan.com/address/0xb68fF1BCCdfd5B8984A5437e5a00A207611f1693)
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,7 +6,7 @@ const main = async () => {
     console.log("Contract deployed to:", domainContract.address);
   
     // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of bananas lol
-    let txn = await domainContract.register("vibers",  {value: hre.ethers.utils.parseEther('0.1')});
+    let txn = await domainContract.register("vibers",  {value: hre.ethers.utils.parseEther('0.3')});
     await txn.wait();
     console.log("Minted domain vibers.ginger");
   
