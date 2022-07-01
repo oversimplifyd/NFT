c00840d73b5970fbad137e242cd67ab55c4ed578
Arotimi Busayo <arotimi.busayo@gmail.com>
2022-07-01 19:08:47 +0200

    Adds withdraw capabilities

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # NFT Domain System on Polygon
 [View on Opensea Testnet](https://testnets.opensea.io/collection/ginger-name-service-v3)
-[View Contract on Mumba Polygon Testnet](https://mumbai.polygonscan.com/address/0xb68fF1BCCdfd5B8984A5437e5a00A207611f1693)
+
+[View Contract on Mumbai Polygon Testnet](https://mumbai.polygonscan.com/address/0xb68fF1BCCdfd5B8984A5437e5a00A207611f1693)
--- scripts/run.js
+++ scripts/run.js
@@ -1,30 +1,51 @@
 const main = async () => {
-    const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    // We pass in "ninja" to the constructor when deploying
-    const domainContract = await domainContractFactory.deploy("ginger");
-    await domainContract.deployed();
-  
-    console.log("Contract deployed to:", domainContract.address);
-  
-    // We're passing in a second variable - value. This is the moneyyyyyyyyyy
-    let txn = await domainContract.register("vibes",  {value: hre.ethers.utils.parseEther('0.1')});
+  const [owner, superCoder] = await hre.ethers.getSigners();
+  const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+  const domainContract = await domainContractFactory.deploy("ninja");
+  await domainContract.deployed();
+
+  console.log("Contract owner:", owner.address);
+
+  // Let's be extra generous with our payment (we're paying more than required)
+  let txn = await domainContract.register("dda",  {value: hre.ethers.utils.parseEther('1234')});
+  await txn.wait();
+
+  // How much money is in here?
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+
+  // Quick! Grab the funds from the contract! (as superCoder)
+  try {
+    txn = await domainContract.connect(superCoder).withdraw();
     await txn.wait();
-  
-    const address = await domainContract.getAddress("vibes");
-    console.log("Owner of domain vibes:", address);
-  
-    const balance = await hre.ethers.provider.getBalance(domainContract.address);
-    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+  } catch(error){
+    console.log("Could not rob contract");
   }
+
+  // Let's look in their wallet so we can compare later
+  let ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+  console.log("Balance of owner before withdrawal:", hre.ethers.utils.formatEther(ownerBalance));
+
+  // Oops, looks like the owner is saving their money!
+  txn = await domainContract.connect(owner).withdraw();
+  await txn.wait();
   
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
-  
-  runMain();
+  // Fetch balance of contract & owner
+  const contractBalance = await hre.ethers.provider.getBalance(domainContract.address);
+  ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+
+  console.log("Contract balance after withdrawal:", hre.ethers.utils.formatEther(contractBalance));
+  console.log("Balance of owner after withdrawal:", hre.ethers.utils.formatEther(ownerBalance));
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
